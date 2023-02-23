4d5f31c14d1fbed119fde81a1bca4bfac1945ecd
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-02-23 21:06:13 +0100

    fix assert

--- src/inf.synth/inf.synth/cv_bank/processor.cpp
+++ src/inf.synth/inf.synth/cv_bank/processor.cpp
@@ -245,7 +245,10 @@ cv_bank_processor::apply_modulation(cv_bank_input const& input,
       in_modified[s] = amt[s] * ((offset[s] + (1.0f - offset[s]) * scale[s] * (in[s] + 1.0f) * 0.5f) * 2.0f - 1.0f);
 
   for (std::int32_t s = 0; s < ss; s++)
-    assert(0.0f <= in_modified[s] && in_modified[s] <= 1.0f);
+  {
+    assert(bipolar || 0.0f <= in_modified[s] && in_modified[s] <= 1.0f);
+    assert(!bipolar || -1.0f <= in_modified[s] && in_modified[s] <= 1.0f);
+  }
 
   // Modulate.
   switch (indices.input_op_index)
