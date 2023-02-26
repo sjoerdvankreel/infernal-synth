dc726f4dbb76564330b1a03725cbc8a79d2ff5bd
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-02-26 18:17:19 +0100

    take 3

--- src/inf.vst.base/inf.vst.base/ui/part_connector.cpp
+++ src/inf.vst.base/inf.vst.base/ui/part_connector.cpp
@@ -61,9 +61,9 @@ part_connector::draw(VSTGUI::CDrawContext* context)
     path->closeSubpath();
     break;  
   case connector_direction::halfway_right_down: 
-    path->beginSubpath(size * 7.0 / 12.0 + 1.0, size / 4.0);
-    path->addLine(size * 11.0 / 12.0, size / 2.0);
-    path->addLine(size * 7.0 / 12.0 + 1.0, size * 3.0 / 4.0);
+    path->beginSubpath(size * 5.0 / 12.0, size * 3.0 / 4.0);
+    path->addLine(size * 11.0 / 12.0, size / 4.0);
+    path->addLine(size * 11.0 / 12.0, size * 3.0 / 4.0); 
     path->closeSubpath();
     break;
   case connector_direction::left_right:
@@ -71,7 +71,7 @@ part_connector::draw(VSTGUI::CDrawContext* context)
     path->addLine(size * 6.0 / 12.0, size / 2.0);
     path->addLine(size * 2.0 / 12.0 + 1.0, size * 3.0 / 4.0);
     path->closeSubpath();  
-    path->beginSubpath(size * 7.0 / 12.0 + 1.0, size / 2.0);
+    path->beginSubpath(size * 7.0 / 12.0 + 1.0, size / 2.0); 
     path->addLine(size * 11.0 / 12.0, size / 5.0);
     path->addLine(size * 11.0 / 12.0, size * 4.0 / 5.0);
     path->closeSubpath();
