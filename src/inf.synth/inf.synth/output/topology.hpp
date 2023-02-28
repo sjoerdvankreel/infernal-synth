ad5ace466fdac5fe28858fcfff02bccfb30ac0e6
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-02-28 18:05:07 +0100

    output borders

--- src/inf.synth/inf.synth/output/topology.hpp
+++ src/inf.synth/inf.synth/output/topology.hpp
@@ -10,6 +10,9 @@
 
 namespace inf::synth {
 
+inline std::vector<base::box_descriptor> const output_borders = 
+{ { 0, 0, 1, 1 }, { 0, 1, 1, 2 }, { 0, 3, 1, 3 } };
+
 struct usage_source_t { enum value { 
   voice, master, vaudio, gaudio, vcv, gcv, 
   env, osc, vlfo, glfo, veffect, geffect, count }; };
@@ -23,5 +26,6 @@ inline float constexpr output_table_col_widths[output_param::count] =
 { 0.12f, 0.19f, 0.12f, 0.15f, 0.21f, 0.21f };
 inline base::part_table_descriptor const output_table = 
 { false, output_param::count, output_table_col_widths, nullptr }; 
+
 } // namespace inf::synth
 #endif // INF_SYNTH_OUTPUT_TOPOLOGY_HPP  
