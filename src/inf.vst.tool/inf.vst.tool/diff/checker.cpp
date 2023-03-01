be089faa83812315c4ed4eeecdef9a70a77ba8e8
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-03-01 18:48:35 +0100

    diff take 4

--- src/inf.vst.tool/inf.vst.tool/diff/checker.cpp
+++ src/inf.vst.tool/inf.vst.tool/diff/checker.cpp
@@ -2,60 +2,52 @@
 #include <inf.vst.tool/shared/load_topology.hpp>
 
 #include <set>
+#include <string>
 #include <fstream>
 #include <iostream>
 
 using namespace inf::base;
 using namespace inf::vst::tool::shared;
 
-namespace inf::vst::tool::diff {
-
 static std::int32_t
-find_param(topology_info const* seek, topology_info const* find, std::int32_t seek_index)
+find_part(topology_info const* seek, topology_info const* find, std::int32_t seek_index)
 {
-  auto const& param_seek = *seek->params[seek_index].descriptor;
-  auto const& part_seek = seek->parts[seek->params[seek_index].part_index];
-  for (std::int32_t i = 0; i < find->params.size(); i++)
-  {
-    auto const& param_find = *find->params[i].descriptor;
-    auto const& part_find = find->parts[find->params[i].part_index];
-    if (part_seek.descriptor->guid == part_find.descriptor->guid &&
-      part_seek.type_index == part_find.type_index &&
-      param_seek.guid == param_find.guid)
-        return i;
-  }
+  for(std::int32_t i = 0; i < find->static_part_count; i++)
+    if(seek->static_parts[seek_index].guid == find->static_parts[i].guid)
+      return i;
   return -1;
 }
+
+namespace inf::vst::tool::diff {
 
 std::int32_t
 diff_plugin(char const* library1_path, char const* library2_path)
 {
-  std::vector<std::string> added_names;
-  std::vector<std::string> removed_names;
   std::unique_ptr<loaded_topology> loaded_topo1 = load_topology(library1_path);
   std::unique_ptr<loaded_topology> loaded_topo2 = load_topology(library2_path);
   if(!loaded_topo1 || !loaded_topo2) return 1;
-
   topology_info const* topo1 = loaded_topo1->topology();
   topology_info const* topo2 = loaded_topo2->topology();
-  
-  for(std::int32_t i = 0; i < topo1->params.size(); i++)
-    if(find_param(topo1, topo2, i) == -1)
-      removed_names.push_back(topo1->params[i].runtime_name);
-  for (std::int32_t i = 0; i < topo2->params.size(); i++)
-    if (find_param(topo2, topo1, i) == -1)
-      added_names.push_back(topo2->params[i].runtime_name);
-  
-  for(std::int32_t i = 0; i < removed_names.size(); i++)
-    if(std::find(added_names.begin(), added_names.end(), removed_names[i]) == added_names.end())
-      std::cout << "Removed " << removed_names[i] << ".\n";
-    else
-      std::cout << "Changed id: " << removed_names[i] << ".\n";
-  for (std::int32_t i = 0; i < added_names.size(); i++)
-    if (std::find(removed_names.begin(), removed_names.end(), added_names[i]) == removed_names.end())
-      std::cout << "Added " << added_names[i] << ".\n";
-    else
-      std::cout << "Changed id: " << added_names[i] << ".\n";
+
+  for(std::int32_t i = 0; i < topo1->static_part_count; i++)
+    if(find_part(topo1, topo2, i) == -1)
+      std::cout << "Removed part " << topo1->static_parts[i].static_name.detail << ".\n";
+  for (std::int32_t i = 0; i < topo2->static_part_count; i++)
+    if (find_part(topo2, topo1, i) == -1)
+      std::cout << "Added part " << topo2->static_parts[i].static_name.detail << ".\n";
+  for (std::int32_t i = 0; i < topo2->static_part_count; i++)
+  {
+    std::int32_t old_index = find_part(topo2, topo1, i);
+    if (old_index < 0) continue;
+    part_descriptor const* part2 = &topo2->static_parts[i];
+    part_descriptor const* part1 = &topo1->static_parts[old_index];
+    if(part1->kind != part2->kind) std::cout << topo2->static_parts[i].static_name.detail << " changed kind.\n";
+    if(part1->type != part2->type) std::cout << topo2->static_parts[i].static_name.detail << " changed type.\n";
+    if(part1->part_count != part2->part_count) std::cout << topo2->static_parts[i].static_name.detail << " changed part count.\n";
+    if(part1->param_count != part2->param_count) std::cout << topo2->static_parts[i].static_name.detail << " changed param count.\n";
+    if(std::string(part1->static_name.detail) != part2->static_name.detail) std::cout << topo2->static_parts[i].static_name.detail << " changed long name.\n";
+    if(std::string(part1->static_name.short_) != part2->static_name.short_) std::cout << topo2->static_parts[i].static_name.detail << " changed short name.\n";
+  }
 
   return 0;
 }
