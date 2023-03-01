f7d39dfb6e7f7c9bc902450b9d1701760fb91d24
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-03-01 19:30:27 +0100

    diff take 5

--- src/inf.vst.tool/inf.vst.tool/diff/checker.cpp
+++ src/inf.vst.tool/inf.vst.tool/diff/checker.cpp
@@ -10,10 +10,25 @@ using namespace inf::base;
 using namespace inf::vst::tool::shared;
 
 static std::int32_t
-find_part(topology_info const* seek, topology_info const* find, std::int32_t seek_index)
+find_part(
+  topology_info const* seek, 
+  topology_info const* find, 
+  std::int32_t seek_part)
 {
   for(std::int32_t i = 0; i < find->static_part_count; i++)
-    if(seek->static_parts[seek_index].guid == find->static_parts[i].guid)
+    if(seek->static_parts[seek_part].guid == find->static_parts[i].guid)
+      return i;
+  return -1;
+}
+
+static std::int32_t
+find_param(
+  topology_info const* seek, std::int32_t seek_part, 
+  topology_info const* find, std::int32_t find_part, 
+  std::int32_t seek_param)
+{
+  for(std::int32_t i = 0; i < find->static_parts[find_part].param_count; i++)
+    if(seek->static_parts[seek_part].params[seek_param].guid == find->static_parts[find_part].params[i].guid)
       return i;
   return -1;
 }
@@ -31,22 +46,33 @@ diff_plugin(char const* library1_path, char const* library2_path)
 
   for(std::int32_t i = 0; i < topo1->static_part_count; i++)
     if(find_part(topo1, topo2, i) == -1)
-      std::cout << "Removed part " << topo1->static_parts[i].static_name.detail << ".\n";
+      std::cout << topo1->static_parts[i].static_name.detail << " removed.\n";
   for (std::int32_t i = 0; i < topo2->static_part_count; i++)
     if (find_part(topo2, topo1, i) == -1)
-      std::cout << "Added part " << topo2->static_parts[i].static_name.detail << ".\n";
+      std::cout << topo2->static_parts[i].static_name.detail << " added.\n";
+  
   for (std::int32_t i = 0; i < topo2->static_part_count; i++)
   {
     std::int32_t old_index = find_part(topo2, topo1, i);
     if (old_index < 0) continue;
+    
+    std::cout << topo2->static_parts[i].static_name.detail << " diff:\n";
     part_descriptor const* part2 = &topo2->static_parts[i];
     part_descriptor const* part1 = &topo1->static_parts[old_index];
-    if(part1->kind != part2->kind) std::cout << topo2->static_parts[i].static_name.detail << " changed kind.\n";
-    if(part1->type != part2->type) std::cout << topo2->static_parts[i].static_name.detail << " changed type.\n";
-    if(part1->part_count != part2->part_count) std::cout << topo2->static_parts[i].static_name.detail << " changed part count.\n";
-    if(part1->param_count != part2->param_count) std::cout << topo2->static_parts[i].static_name.detail << " changed param count.\n";
-    if(std::string(part1->static_name.detail) != part2->static_name.detail) std::cout << topo2->static_parts[i].static_name.detail << " changed long name.\n";
-    if(std::string(part1->static_name.short_) != part2->static_name.short_) std::cout << topo2->static_parts[i].static_name.detail << " changed short name.\n";
+    if(part1->kind != part2->kind) std::cout << "\tChanged kind.\n";
+    if(part1->type != part2->type) std::cout << "\tChanged type.\n";
+    if(part1->param_count != part2->param_count) "\tChanged param count.\n";
+    if(std::string(part1->static_name.detail) != part2->static_name.detail) std::cout << "\tChanged long name.\n";
+    if(std::string(part1->static_name.short_) != part2->static_name.short_) std::cout << "\tChanged short name.\n";
+    if(part1->part_count != part2->part_count) std::cout << "\tChanged part count from " << part1->part_count << " to " << part2->part_count << ".\n";
+
+    std::cout << "\tParam diff:\n";
+    for(std::int32_t j = 0; j < topo1->static_parts[old_index].param_count; j++)
+      if(find_param(topo1, old_index, topo2, i, j) == -1)
+        std::cout << "\t\t" << topo1->static_parts[old_index].params[j].data.static_name.detail << " removed.\n";
+    for (std::int32_t j = 0; j < topo2->static_parts[i].param_count; j++)
+      if (find_param(topo2, i, topo1, old_index, j) == -1)
+        std::cout << "\t\t" << topo2->static_parts[i].params[j].data.static_name.detail << " added.\n";
   }
 
   return 0;
