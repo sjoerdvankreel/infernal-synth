be5f37d9968f68c0105274e2f3fa98bc4c4f5b11
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-03-08 21:39:26 +0100

    update build script

--- scripts/make_win32.bat
+++ scripts/make_win32.bat
@@ -4,7 +4,7 @@ mkdir build
 cd build
 mkdir win32
 cd win32
-cmake ../../ -DVST_SDK_377_SRC_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk -DVST_SDK_377_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk/build/lib/Debug -DVST_SDK_377_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk/build/lib/Release -DHIIR_140_SRC_DIR=C:/repos/hiir_1.40
+cmake ../../ -DVST_SDK_377_SRC_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk_dynamic -DVST_SDK_377_BUILD_DEBUG_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk_dynamic/build/lib/Debug -DVST_SDK_377_BUILD_RELEASE_DIR=C:/repos/vst-sdk_3.7.7/win32/vst3sdk_dynamic/build/lib/Release -DHIIR_140_SRC_DIR=C:/repos/hiir_1.40
 cd ..
 cd ..
 cd scripts
