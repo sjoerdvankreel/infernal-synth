ce15e364307c3c9ef7c932708b995f98f8db69fa
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 01:01:22 +0200

    update build scripts

--- scripts/gen_vstui.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-..\build\win32\Debug\infernal.tool.vst.exe --generate-ui ..\build\win32\Debug\InfernalSynth\Generic\InfernalSynth.vst3\Contents\x86_64-win\InfernalSynth.vst3 ..\ui\controller.uidesc
