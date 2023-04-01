5328654636525261d6cf3b8633d4561cc340d96f
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 01:22:06 +0200

    build script

--- scripts/make_linux.sh
+++ scripts/make_linux.sh
@@ -17,9 +17,9 @@ cd ..
 mkdir -p release
 cd release
 cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DVST_SDK_377_SRC_DIR=/media/sf_vst-sdk_3.7.7/vst3sdk -DVST_SDK_377_BUILD_DEBUG_DIR=/media/sf_vst-sdk_3.7.7/linux_debug/lib/Debug -DVST_SDK_377_BUILD_RELEASE_DIR=/media/sf_vst-sdk_3.7.7/linux_release/lib/Release -DVSTGUI_FORK_SRC_DIR=/media/sf_vstgui_fork/vstgui -DVSTGUI_FORK_BUILD_DEBUG_DIR=/media/sf_vstgui_fork/linux_debug/Debug/libs -DVSTGUI_FORK_BUILD_RELEASE_DIR=/media/sf_vstgui_fork/linux_release/Release/libs -DHIIR_140_SRC_DIR=/media/sf_hiir_1.40
+make
 strip --strip-unneeded ../../../dist/Release/InfernalSynth1.1.vst3/Contents/x86_64-linux/InfernalSynth1.1.so
 strip --strip-unneeded ../../../dist/Release/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so
-make
 cd ..
 
 cd ..
