3227d396471dca34f5cb0e14fdced4097be8c2f8
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 15:22:56 +0200

    build script

--- scripts/setup_vst3sdk_win32.bat
+++ scripts/setup_vst3sdk_win32.bat
@@ -6,14 +6,10 @@ git submodule set-url -- vstgui4 https://github.com/sjoerdvankreel/vstgui.git
 cd vstgui4
 git checkout develop
 git pull
-cd ..
-cd ..
-if not exist "build" mkdir build
-cd build
-if not exist "vst3sdk" mkdir vst3sdk
-cd vst3sdk 
-if not exist "win32" mkdir win32
-cd win32
+cd ..\..
+if not exist "build\vst3sdk\win32" mkdir build\vst3sdk\win32
+cd build\vst3sdk\win32
 cmake -DVSTGUI_ENABLE_INFERNAL=1 ../../../vst3sdk
 msbuild /property:Configuration=Debug vstsdk.sln
 msbuild /property:Configuration=Release vstsdk.sln
+cd ..\..\..\scripts
