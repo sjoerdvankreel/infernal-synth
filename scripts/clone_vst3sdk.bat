d27c50da4c6f9899be7c28a2e3a00909a2dc8e37
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 14:49:53 +0200

    build script

--- scripts/clone_vst3sdk.bat
+++
@@ -1,6 +0,0 @@
-git clone --recursive https://github.com/steinbergmedia/vst3sdk.git
-cd vst3sdk
-git submodule set-url -- vstgui4 https://github.com/sjoerdvankreel/vstgui.git
-cd vstgui4
-git checkout develop
-git pull
---
+++ scripts/setup_vst3sdk_win32.bat
@@ -0,0 +1,14 @@
+if not exist "vst3sdk" git clone --recursive https://github.com/steinbergmedia/vst3sdk.git
+cd vst3sdk
+git checkout v3.7.7_build_19
+git submodule set-url -- vstgui4 https://github.com/sjoerdvankreel/vstgui.git
+cd vstgui4
+git checkout develop
+git pull
+cd ..
+cd ..
+mkdir win32
+cd win32
+cmake ../vst3sdk
+msbuild /property:Configuration=Debug vstsdk.sln
+msbuild /property:Configuration=Release vstsdk.sln
