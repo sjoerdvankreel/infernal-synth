6d72a012a789ceacf39a8d26fb3c400d7bcf3cb6
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 16:49:17 +0200

    build script

--- scripts/build_win32.bat
+++ scripts/build_win32.bat
@@ -1,7 +1,7 @@
 cd ..
 if not exist build\infernal\win32 mkdir build\infernal\win32
 cd build\infernal\win32
-cmake ../../../ -DHIIR_140_SRC_DIR="%1"
+cmake -DVSTGUI_ENABLE_INFERNAL=1 -DHIIR_140_SRC_DIR="%1" ../../../
 msbuild /property:Configuration=Debug infernal-synth.sln
 msbuild /property:Configuration=Release infernal-synth.sln
 cd ..\..\..\scripts
