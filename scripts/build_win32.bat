1da8964395913bc4139522351417a3fdef2c2daa
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 16:22:01 +0200

    build script

---
+++ scripts/build_win32.bat
@@ -0,0 +1,7 @@
+cd ..
+if not exist build\infernal\win32 mkdir build\infernal\win32
+cd build\infernal\win32
+cmake ../../../ -DHIIR_140_SRC_DIR="%1"
+msbuild /property:Configuration=Debug infernal-synth.sln
+msbuild /property:Configuration=Release infernal-synth.sln
+cd ..\..\..\scripts
