12adc71d621c86f03bff2d534d3b1b5220cd8cd6
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 20:17:19 +0200

    build script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -12,7 +12,7 @@ mkdir -p build/infernal/linux/release
 cd build/infernal/linux/release
 cmake -DCMAKE_BUILD_TYPE=Release -DHIIR_140_SRC_DIR="$1" ../../../../
 make
-strip --strip-unneeded ../../../../dist/Release/InfernalSynth1.1.vst3/Contents/x86_64-linux/InfernalSynth1.1.so
-strip --strip-unneeded ../../../../dist/Release/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so
+strip --strip-unneeded ../../../../dist/linux/Release/InfernalSynth1.1.vst3/Contents/x86_64-linux/InfernalSynth1.1.so
+strip --strip-unneeded ../../../../dist/linux/Release/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so
 
 cd ../../../../scripts
