42407499e705f98d071bdf9b5f17c95f168e6735
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 16:11:29 +0200

    build script

--- scripts/setup_vst3sdk_win32.bat
+++ scripts/setup_vst3sdk_win32.bat
@@ -9,7 +9,7 @@ git pull
 cd ..\..
 if not exist "build\vst3sdk\win32" mkdir build\vst3sdk\win32
 cd build\vst3sdk\win32
-cmake -DVSTGUI_ENABLE_INFERNAL=1 ../../../vst3sdk
+cmake -DVSTGUI_ENABLE_INFERNAL=1 -DSMTG_ADD_VST3_PLUGINS_SAMPLES=OFF -DSMTG_ADD_VST3_HOSTING_SAMPLES=OFF -DSMTG_ADD_VST3_UTILITIES=OFF -DSMTG_CREATE_MODULE_INFO=OFF -DSMTG_CREATE_PLUGIN_LINK=OFF -DSMTG_RUN_VST_VALIDATOR=OFF -DVSTGUI_ENABLE_XMLPARSER=OFF ../../../vst3sdk
 msbuild /property:Configuration=Debug vstsdk.sln
 msbuild /property:Configuration=Release vstsdk.sln
 cd ..\..\..\scripts
