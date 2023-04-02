302c24788f7e91875f6946ffebff541df63815cf
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 15:04:10 +0200

    build script

--- scripts/setup_vst3sdk_win32.bat
+++ scripts/setup_vst3sdk_win32.bat
@@ -1,8 +1,4 @@
 cd ..
-if not exist "vst3sdk" mkdir vst3sdk
-cd vst3sdk
-if not exist "src" mkdir src
-cd src
 if not exist "vst3sdk" git clone --recursive https://github.com/steinbergmedia/vst3sdk.git
 cd vst3sdk
 git checkout v3.7.7_build_19
@@ -12,11 +8,12 @@ git checkout develop
 git pull
 cd ..
 cd ..
-cd ..
 if not exist "build" mkdir build
 cd build
+if not exist "vst3sdk" mkdir vst3sdk
+cd vst3sdk 
 if not exist "win32" mkdir win32
 cd win32
-cmake ../../src/vst3sdk
+cmake ../../../vst3sdk
 msbuild /property:Configuration=Debug vstsdk.sln
 msbuild /property:Configuration=Release vstsdk.sln
