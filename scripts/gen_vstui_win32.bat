ad8293adef47387e07336158aa80aea5158684a3
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-02 22:06:37 +0200

    build script

--- scripts/gen_vstui_win32.bat
+++ scripts/gen_vstui_win32.bat
@@ -1,2 +1,2 @@
 @echo off
-..\build\win32\Debug\infernal.tool.vst.exe --generate-ui ..\dist\Debug\InfernalSynthGeneric.vst3\Contents\x86_64-win\InfernalSynthGeneric.vst3 ..\ui\controller.uidesc
+..\build\infernal\win32\Debug\infernal.tool.vst.exe --generate-ui ..\dist\win32\Debug\InfernalSynthGeneric.vst3\Contents\x86_64-win\InfernalSynthGeneric.vst3 ..\ui\controller.uidesc
