9f68c73d52f9f65bef480e841eaa73e8b73b34f8
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-03 19:48:33 +0200

    build script

--- scripts/build_infernal_win32.bat
+++ scripts/build_infernal_win32.bat
@@ -1,12 +1,18 @@
 @echo off
+setlocal
 if [%1] == [] goto usage
 
 cd ..
 if not exist build\infernal\win32 mkdir build\infernal\win32
 cd build\infernal\win32
 cmake -DHIIR_140_SRC_DIR="%1" ../../../
+if %errorlevel% neq 0 exit /b !errorlevel!
+
 msbuild /property:Configuration=Debug infernal-synth.sln
+if %errorlevel% neq 0 exit /b !errorlevel!
 msbuild /property:Configuration=Release infernal-synth.sln
+if %errorlevel% neq 0 exit /b !errorlevel!
+
 cd ..\..\..\scripts
 goto :eof
 
--- scripts/build_win32.bat
+++ scripts/build_win32.bat
@@ -1,8 +1,11 @@
 @echo off
+setlocal
 if [%1] == [] goto usage
 
 setup_vst3sdk_win32
+if %errorlevel% neq 0 exit /b !errorlevel!
 build_infernal_win32 %1
+if %errorlevel% neq 0 exit /b !errorlevel!
 goto :eof
 
 :usage
