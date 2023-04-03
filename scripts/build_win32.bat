4c38670a0eb9537bbf57dd61f0ab59e87df35eda
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-03 19:28:25 +0200

    build script

--- scripts/build_win32.bat
+++ scripts/build_win32.bat
@@ -1,3 +1,6 @@
+@echo off
+if [%1] == [] goto usage
+
 cd ..
 if not exist build\infernal\win32 mkdir build\infernal\win32
 cd build\infernal\win32
@@ -5,3 +8,8 @@ cmake -DHIIR_140_SRC_DIR="%1" ../../../
 msbuild /property:Configuration=Debug infernal-synth.sln
 msbuild /property:Configuration=Release infernal-synth.sln
 cd ..\..\..\scripts
+goto :eof
+
+:usage
+echo "Usage: build_win32 <path-to-hiir>"
+exit /B 1
