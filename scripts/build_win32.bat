fd522986a230621892222d8bf9618337eeb90442
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-03 20:13:08 +0200

    build script

--- scripts/build_win32.bat
+++ scripts/build_win32.bat
@@ -2,12 +2,15 @@
 setlocal
 if [%1] == [] goto usage
 
-setup_vst3sdk_win32
+echo Setting up vst sdk...
+call setup_vst3sdk_win32
 if %errorlevel% neq 0 exit /b !errorlevel!
-build_infernal_win32 %1
-if %errorlevel% neq 0 exit /b !errorlevel!
-goto :eof
 
+echo Building infernal...
+call build_infernal_win32 %1
+if %errorlevel% neq 0 exit /b !errorlevel!
+
+goto :eof
 :usage
 echo "Usage: %0 <path-to-hiir>"
 exit /B 1
