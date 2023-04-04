ebed4089df847c45d05ba3420f5a0eee6eb3c0e2
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-04 22:07:30 +0200

    Revert "allow left-click on empty space to popup the context menu (working in studio one)"
    This reverts commit 151051664b0c092d1ba137f97e04820d19dfd7c3.

--- src/inf.base.vst.ui/inf.base.vst.ui/vst_ui_editor.cpp
+++ src/inf.base.vst.ui/inf.base.vst.ui/vst_ui_editor.cpp
@@ -6,7 +6,6 @@
 #include <cassert>
 #include <algorithm>
 
-using namespace Steinberg;
 using namespace VSTGUI;
 using namespace inf::base;
 using namespace inf::base::ui;
@@ -46,49 +45,6 @@ vst_ui_editor::open(void* parent, const PlatformType& type)
   if (!VST3Editor::open(parent, type)) return false;
   dynamic_cast<vst_ui_controller&>(*getController()).sync_ui_parameters();
   return true;
-}
-
-// Force context menu open on left-click on stuff that's not a control.
-// At least studio one (maybe other hosts) dont like opening context menu outside a control.
-void
-vst_ui_editor::onMouseEvent(MouseEvent& event, CFrame* which_frame)
-{
-  if (event.type != EventType::MouseDown || event.buttonState.isRight())
-  {
-    // Not a left-mouse event, do default behaviour.
-    VST3Editor::onMouseEvent(event, which_frame);
-    return;
-  }
-
-  ParamID tag;
-  tresult found = find_parameter(event.mousePosition, tag);
-  if(found == kResultOk)
-  {
-    // Clicked on a parameter, do default behaviour.
-    VST3Editor::onMouseEvent(event, which_frame);
-    return;
-  }
-
-  CPoint size = getFrame()->getViewSize().getSize();
-  if (event.mousePosition.x <= 0.0 
-  || event.mousePosition.y <= 0.0 
-  || event.mousePosition.x >= size.x 
-  || event.mousePosition.y >= size.y)
-  {
-    // Outside plugin window, do default behaviour.
-    VST3Editor::onMouseEvent(event, which_frame);
-    return;
-  }
-
-  // Force popup controller context menu without appending host menu items.
-  SharedPointer<CFrame> frame_owner = getFrame();
-  COptionMenu* menu = dynamic_cast<vst_ui_controller&>(*getController()).createContextMenu(event.mousePosition, this);
-  getFrame()->doAfterEventProcessing([=, position = event.mousePosition]() {
-    menu->setStyle(COptionMenu::kPopupStyle);
-    menu->popup(frame_owner, position);
-    menu->forget();
-  });
-  event.consumed = true;
 }
 
 } // namespace inf::base::vst::ui
