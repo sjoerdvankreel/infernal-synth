a3e8bc8c58acda78d7189dc81d01937c3cbcb8b1
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-05 17:54:37 +0200

    build script

--- scripts/gen_vstui_linux.sh
+++ scripts/gen_vstui_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-../build/linux/debug/infernal.tool.vst --generate-ui ../dist/Debug/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so ../ui/controller.uidesc
+../build/infernal/linux/debug/infernal.tool.vst --generate-ui ../dist/linux/Debug/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so ../ui/controller.uidesc
