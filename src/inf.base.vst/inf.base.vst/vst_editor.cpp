3501949c7b8fb8f810d456695044efd2a0416141
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-10 15:25:58 +0200

    empty editor window

--- src/inf.base.vst/inf.base.vst/vst_editor.cpp
+++ src/inf.base.vst/inf.base.vst/vst_editor.cpp
@@ -1,6 +1,8 @@
 #include <inf.base.vst/vst_editor.hpp>
 #include <inf.base.vst/vst_controller.hpp>
 
+#include <cstring>
+
 using namespace Steinberg;
 
 namespace inf::base::vst {
@@ -9,5 +11,22 @@ vst_editor::
 vst_editor(vst_controller* controller):
 EditorView(controller)
 { assert(controller != nullptr); }
+
+tresult PLUGIN_API 
+vst_editor::attached(void* parent, FIDString type)
+{
+  ViewRect vr(0, 0, 100, 100);
+  setRect(vr);
+  if(plugFrame) plugFrame->resizeView(this, &vr);
+  return EditorView::attached(parent, type);
+}
+
+tresult PLUGIN_API 
+vst_editor::isPlatformTypeSupported(FIDString type)
+{
+  if(std::strcmp(type, kPlatformTypeHWND) == 0) return kResultTrue;
+  if(std::strcmp(type, kPlatformTypeX11EmbedWindowID) == 0) return kResultTrue;
+  return kResultFalse;
+}
 
 } // namespace inf::base::vst
--- src/inf.base.vst/inf.base.vst/vst_editor.hpp
+++ src/inf.base.vst/inf.base.vst/vst_editor.hpp
@@ -10,8 +10,13 @@ class vst_controller;
 class vst_editor: 
 public Steinberg::Vst::EditorView
 {
+  using tresult = Steinberg::tresult;
+  using FIDString = Steinberg::FIDString;
+
 public:
   vst_editor(vst_controller* controller);
+  tresult PLUGIN_API attached(void* parent, FIDString type) override;
+  tresult PLUGIN_API isPlatformTypeSupported(FIDString type) override;
 };
 
 } // namespace inf::base::vst
