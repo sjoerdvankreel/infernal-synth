135a47619ca31f9bc419b1fc134c7f60cb7504f1
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-10 14:08:45 +0200

    drop vstgui from tool

--- src/inf.tool.vst/inf.tool.vst/main.cpp
+++ src/inf.tool.vst/inf.tool.vst/main.cpp
@@ -1,10 +1,8 @@
 #include <inf.tool.vst/diff/checker.hpp>
-#include <inf.tool.vst/ui/generator/generator.hpp>
 
 #include <cstring>
 #include <iostream>
 
-using namespace inf::tool::vst::ui;
 using namespace inf::tool::vst::diff;
 
 int
@@ -12,16 +10,6 @@ main(int argc, char** argv)
 {
   if(argc < 2) 
     return std::cout << "Usage: infernal.vst.tool <options>\n", 1;
-
-  // Generate .uidesc from topology.
-  if (!strcmp(argv[1], "--generate-ui"))
-  {
-    if(argc != 4)
-      return std::cout << 
-        "Usage: infernal.vst.tool --generate-ui " 
-        "<full\\path\\to\\plugin.vst3> (dll) <full\\path\\to\\file.uidesc> (json)\n", 1;
-    return write_vstgui_json(argv[2], argv[3]);
-  }
 
   // Diffs different plugin versions.
   if (!strcmp(argv[1], "--diff-plugin"))
