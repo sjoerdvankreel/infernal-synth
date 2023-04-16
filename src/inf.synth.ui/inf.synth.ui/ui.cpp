e10782ea97d1c35dec1a1b4acabf9a3dfd20a3ac
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-16 12:36:17 +0200

    ui

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -13,9 +13,9 @@ static std::unique_ptr<grid_element>
 create_content(plugin_controller* controller)
 {
   auto result = create_grid_ui(controller, 1, 4, 1.0);
-  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::amt1, 0, 0, 1, 1)->color(Slider::ColourIds::backgroundColourId, Colours::red);
-  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::bal1, 0, 1, 1, 1)->color(Slider::ColourIds::backgroundColourId, Colours::green);
-  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::amt2, 0, 2, 1, 2)->color(Slider::ColourIds::backgroundColourId, Colours::blue);
+  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::amt1, 0, 0, 1, 1)->color(Slider::ColourIds::rotarySliderFillColourId, Colours::red);
+  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::bal1, 0, 1, 1, 1)->color(Slider::ColourIds::rotarySliderFillColourId, Colours::green);
+  add_grid_param_cell(result.get(), part_type::vaudio_bank, 0, audio_bank_param::amt2, 0, 2, 1, 2)->color(Slider::ColourIds::rotarySliderFillColourId, Colours::blue);
   return result;
 }
 
