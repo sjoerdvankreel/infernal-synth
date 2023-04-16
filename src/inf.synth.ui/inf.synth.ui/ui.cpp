ef45e3353567e10ee942e1140311b1b438db8d51
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-16 22:21:58 +0200

    ui

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -65,8 +65,8 @@ create_synth_ui(std::int32_t width)
   result->content(create_content());
   result->look_and_feel().setColour(Slider::ColourIds::thumbColourId, Colour(0xFFFD9A4D));
   result->look_and_feel().setColour(inf_look_and_feel::colors::knob_shadow, Colour(0xFF000000));
-  result->look_and_feel().setColour(inf_look_and_feel::colors::knob_highlight, Colour(0xFFFFFFFF));
-  result->look_and_feel().setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::darkgrey);
+  result->look_and_feel().setColour(inf_look_and_feel::colors::knob_highlight, Colour(0xFFC0C0C0));
+  result->look_and_feel().setColour(Slider::ColourIds::rotarySliderFillColourId, Colour(0xFF606060));
   result->look_and_feel().setColour(Slider::ColourIds::rotarySliderOutlineColourId, Colour(0xFF29A5BB));
   return result;
 }
