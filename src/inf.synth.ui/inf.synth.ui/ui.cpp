1d21630d7cde46f5bd5c176cb1e8ba51d6365e71
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-17 20:43:31 +0200

    ui

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -77,7 +77,7 @@ create_synth_ui(std::int32_t width)
   result->look_and_feel().setColour(inf_look_and_feel::colors::knob_highlight, Colour(0xFFCCCCCC));
   result->look_and_feel().setColour(inf_look_and_feel::colors::knob_fill_base, Colour(0xFF222222));
   result->look_and_feel().setColour(inf_look_and_feel::colors::knob_cuts_inward, Colour(0xFF660000));
-  result->look_and_feel().setColour(inf_look_and_feel::colors::knob_cuts_outward, Colour(0x00660000));
+  result->look_and_feel().setColour(inf_look_and_feel::colors::knob_cuts_outward, Colour(0x40660000));
   result->look_and_feel().setColour(inf_look_and_feel::colors::knob_fill_highlight, Colour(0xFFCCCCCC));
   return result;
 }
