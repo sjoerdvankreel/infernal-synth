959a4c8f6e154ea576c0c542ffa4912300578402
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-04-23 15:56:39 +0200

    ui

--- src/inf.base.ui/inf.base.ui/text_param_listener.cpp
+++ src/inf.base.ui/inf.base.ui/text_param_listener.cpp
@@ -11,7 +11,8 @@ text_param_listener::param_changed(param_value value)
 {
   topology_info const* topology = _controller->topology();
   auto const desc = topology->params[_param_index].descriptor;
-  _label->setText(desc->data.format(false, value), dontSendNotification);
+  std::string display_value = desc->data.format(false, value);
+  _label->setText(display_value + desc->data.unit, dontSendNotification);
 }
 
 } // namespace inf::base::ui
