a9314a0535863b6f068fa4a7f19a3c0ead987796
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-07 22:36:47 +0200

    dsf group

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -101,13 +101,11 @@ create_osc_mix_group(plugin_controller* controller)
 static std::unique_ptr<ui_element>
 create_osc_dsf_group(plugin_controller* controller)
 {
-  auto outer_grid = create_grid_ui(controller, 1, 5);
-  outer_grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_parts, edit_type::knob, label_type::label), 0, 0);
-  auto inner_grid = create_grid_ui(controller, 2, 1);
-  inner_grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_dist, edit_type::hslider, label_type::label), 0, 0);
-  inner_grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_decay, edit_type::hslider, label_type::label), 1, 0);
-  outer_grid->add_cell(std::move(inner_grid), 0, 1, 1, 4);
-  return create_group_ui(controller, create_group_label_ui(controller, "DSF", true), std::move(outer_grid));
+  auto grid = create_grid_ui(controller, 3, 1);
+  grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_parts, edit_type::hslider, label_type::label), 0, 0);
+  grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_dist, edit_type::hslider, label_type::label), 1, 0);
+  grid->add_cell(create_labeled_param_ui(controller, part_type::vosc, 0, osc_param::dsf_decay, edit_type::hslider, label_type::label), 2, 0);
+  return create_group_ui(controller, create_group_label_ui(controller, "DSF", true), std::move(grid));
 }
 
 static std::unique_ptr<ui_element>
