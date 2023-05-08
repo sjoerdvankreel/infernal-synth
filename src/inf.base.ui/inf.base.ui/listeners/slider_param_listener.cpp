8358cfdde4c1f6fcdeae8b8130773adb2963b4e5
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-08 21:00:49 +0200

    tooltip

--- src/inf.base.ui/inf.base.ui/listeners/slider_param_listener.cpp
+++ src/inf.base.ui/inf.base.ui/listeners/slider_param_listener.cpp
@@ -11,7 +11,7 @@ slider_param_listener::controller_param_changed(param_value ui_value)
 {
   auto const& data = _controller->topology()->params[_param_index].descriptor->data;
   if (data.type == param_type::real) _slider->setValue(ui_value.real);
-  else _slider->setValue(ui_value.discrete);
+  else _slider->setValue(ui_value.discrete, dontSendNotification);
 }
 
 void
