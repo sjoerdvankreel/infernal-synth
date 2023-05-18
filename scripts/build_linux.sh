ecabb3dfd2087e6ed748d12ae4a9323110ac8259
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-18 17:12:26 +0200

    switch over to juce's bundles vst3 sdk

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -ne 5 ]
+if [ "$#" -ne 2 ]
 then
-  echo "Usage: $0 <path-to-hiir-src> <path-to-juce-src> <path-to-vst3sdk-src> <path-to-vst3sdk-debug-binaries> <path-to-vst3sdk-release-binaries>"
+  echo "Usage: $0 <path-to-hiir-src> <path-to-juce-src>"
   exit 1
 fi
 
 cd ..
 mkdir -p build/linux/debug
 cd build/linux/debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DHIIR_140_SRC_DIR="$1" -DJUCE_705_SRC_DIR="$2" -DVST3_SDK_SRC_DIR="$3" -DVST3_SDK_BUILD_DEBUG_DIR="$4" -DVST3_SDK_BUILD_RELEASE_DIR="$5" ../../..
+cmake -DCMAKE_BUILD_TYPE=Debug -DHIIR_140_SRC_DIR="$1" -DJUCE_705_SRC_DIR="$2" ../../..
 make
 
 cd ..
 mkdir -p release
 cd release
-cmake -DCMAKE_BUILD_TYPE=Release -DHIIR_140_SRC_DIR="$1" -DJUCE_705_SRC_DIR="$2" -DVST3_SDK_SRC_DIR="$3" -DVST3_SDK_BUILD_DEBUG_DIR="$4" -DVST3_SDK_BUILD_RELEASE_DIR="$5" ../../..
+cmake -DCMAKE_BUILD_TYPE=Release -DHIIR_140_SRC_DIR="$1" -DJUCE_705_SRC_DIR="$2" ../../..
 make
 strip --strip-unneeded ../../../dist/linux/Release/InfernalSynth1.2.vst3/Contents/x86_64-linux/InfernalSynth1.2.so
 strip --strip-unneeded ../../../dist/linux/Release/InfernalSynthGeneric.vst3/Contents/x86_64-linux/InfernalSynthGeneric.so
