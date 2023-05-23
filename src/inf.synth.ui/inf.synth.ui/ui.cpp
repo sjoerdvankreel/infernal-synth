f47e9c0c634d48de2050e74d03a590f895c50085
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-23 23:21:48 +0200

    lfo

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -345,14 +345,14 @@ create_lfo_lfo_group(plugin_controller* controller, std::int32_t part_type, std:
 static std::unique_ptr<ui_element>
 create_lfo_basic_group(plugin_controller* controller, std::int32_t part_type, std::int32_t part_index)
 {
-  auto knob_grid = create_grid_ui(controller, 2, 1);
+  auto knob_grid = create_grid_ui(controller, 4, 1);
   knob_grid->add_cell(create_param_edit_ui(controller, part_type, part_index, lfo_param::basic_type, edit_type::selector, false), 0, 0);
   knob_grid->add_cell(create_param_icon_ui(controller, part_type, part_index, lfo_param::basic_type, icon_for_lfo_basic_type), 1, 0);
-  auto slider_grid = create_grid_ui(controller, 12, 1);
+  auto slider_grid = create_grid_ui(controller, 24, 1);
   slider_grid->add_cell(create_labeled_param_ui(controller, part_type, part_index, lfo_param::basic_offset, edit_type::hslider, label_type::label, true, false, 8), 1, 0, 4, 1);
   auto pw = slider_grid->add_cell(create_labeled_param_ui(controller, part_type, part_index, lfo_param::basic_pw, edit_type::hslider, label_type::label, true, false, 8), 7, 0, 4, 1);
   pw->relevant_if(part_id(part_type, part_index), lfo_param::basic_type, false, [](std::int32_t part_index, std::int32_t val) { return val == lfo_basic_type::pulse; });
-  auto grid = create_grid_ui(controller, 2, 4);
+  auto grid = create_grid_ui(controller, 1, 4);
   grid->add_cell(std::move(knob_grid), 0, 0, 1, 1);
   grid->add_cell(std::move(slider_grid), 0, 1, 1, 3);
   return create_part_group_ui(controller, create_group_label_ui(controller, "Basic", false), std::move(grid));
