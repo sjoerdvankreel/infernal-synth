f54c80ff486087d8561760ae232340b434317e0b
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-23 22:07:34 +0200

    lfo

--- src/inf.synth.ui/inf.synth.ui/ui.cpp
+++ src/inf.synth.ui/inf.synth.ui/ui.cpp
@@ -372,7 +372,7 @@ create_lfo_random_group(plugin_controller* controller, std::int32_t part_type, s
   auto seedx = other_grid->add_cell(create_labeled_param_ui(controller, part_type, part_index, lfo_param::rand_seedx, edit_type::hslider, label_type::label, true, false, 8), 13, 0, 4, 1);
   seedx->relevant_if(part_id(part_type, part_index), lfo_param::rand_rand_steps, false, [](std::int32_t part_index, std::int32_t val) { return val != 0; });
   auto rand_steps_grid = create_grid_ui(controller, 1, 8);
-  rand_steps_grid->add_cell(create_param_label_ui(controller, part_type, part_index, lfo_param::rand_rand_steps, label_type::label, false), 0, 0);
+  rand_steps_grid->add_cell(create_param_label_ui(controller, part_type, part_index, lfo_param::rand_rand_steps, label_type::label, Justification::right), 0, 0);
   rand_steps_grid->add_cell(create_param_edit_ui(controller, part_type, part_index, lfo_param::rand_rand_steps, edit_type::toggle, false), 0, 1);
   other_grid->add_cell(std::move(rand_steps_grid), 19, 0, 4, 1);
   auto grid = create_grid_ui(controller, 1, 4);
