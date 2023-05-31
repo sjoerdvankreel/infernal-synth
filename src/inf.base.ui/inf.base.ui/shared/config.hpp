508bef3a1ef5b51af9cd34a74f2d1480242e832b
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-05-31 20:27:59 +0200

    stuff

--- src/inf.base.ui/inf.base.ui/shared/config.hpp
+++ src/inf.base.ui/inf.base.ui/shared/config.hpp
@@ -19,24 +19,24 @@ inline juce::Point<int> container_selector_padding = { 10, 8 };
 inline float const selector_min_height = 26.0f;
 inline float const selector_max_height = 42.0f;
 inline float const selector_font_min_height = 11.0f;
-inline float const selector_font_max_height = 14.0f;
+inline float const selector_font_max_height = 16.0f;
 
 inline float const dropdown_hpad = 4.0f;
 inline float const dropdown_vpad_min = 6.0f;
-inline float const dropdown_vpad_max = 32.0f;
+inline float const dropdown_vpad_max = 28.0f;
 inline float const dropdown_item_vpad_min = 6.0f;
-inline float const dropdown_item_vpad_max = 12.0f;
+inline float const dropdown_item_vpad_max = 10.0f;
 inline float const dropdown_font_min_height = 10.0f;
-inline float const dropdown_font_max_height = 12.0f;
+inline float const dropdown_font_max_height = 15.0f;
 inline int   const dropdown_max_column_items = 20;
 
 inline float const param_label_vpad = 4.0f;
 inline float const param_label_font_min_height = 10.0f;
-inline float const param_label_font_max_height = 12.0f;
+inline float const param_label_font_max_height = 15.0f;
 
 inline float const group_label_vpad = 7.0f;
 inline float const group_label_font_min_height = 11.0f;
-inline float const group_label_font_max_height = 14.0f;
+inline float const group_label_font_max_height = 16.0f;
 
 } // namespace inf::base::ui
 #endif // INF_BASE_UI_SHARED_CONFIG_HPP
