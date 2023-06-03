7bca1588ab127602f456bb6641bc537e4dc6b005
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-06-03 17:28:27 +0200

    Revert "test"
    This reverts commit 9ab43bddbf4f1e6f6491eea32ea5cd2f8a4883fe.

--- src/inf.base.ui/inf.base.ui/shared/config.hpp
+++ src/inf.base.ui/inf.base.ui/shared/config.hpp
@@ -14,37 +14,37 @@ inline float const container_radius = 8.0f;
 inline float const container_outline_size = 1.0f;
 
 inline float const toggle_max_size_min = 18.0f;
-inline float const toggle_max_size_max = 38.0f;
+inline float const toggle_max_size_max = 34.0f;
 
 inline juce::Point<int> container_part_padding = { 2, 2 };
 inline juce::Point<int> container_selector_padding = { 10, 8 };
 
 inline float const selector_min_height = 26.0f;
-inline float const selector_max_height = 44.0f;
+inline float const selector_max_height = 40.0f;
 inline float const selector_vlabel_pad_min = 2.0f;
-inline float const selector_vlabel_pad_max = 7.0f;
+inline float const selector_vlabel_pad_max = 6.0f;
 inline float const selector_font_min_height = 11.0f;
-inline float const selector_font_max_height = 17.0f;
+inline float const selector_font_max_height = 15.0f;
 
 inline float const dropdown_hpad = 4.0f;
 inline float const dropdown_vpad_min = 6.0f;
-inline float const dropdown_vpad_max = 19.0f;
+inline float const dropdown_vpad_max = 17.0f;
 inline float const dropdown_item_vpad_min = 6.0f;
-inline float const dropdown_item_vpad_max = 10.0f;
+inline float const dropdown_item_vpad_max = 9.0f;
 inline float const dropdown_font_min_height = 10.0f;
-inline float const dropdown_font_max_height = 16.0f;
+inline float const dropdown_font_max_height = 14.0f;
 inline int   const dropdown_max_column_items = 20;
 
 inline float const param_label_vpad = 4.0f;
 inline float const param_label_font_min_height = 10.0f;
-inline float const param_label_font_max_height = 16.0f;
+inline float const param_label_font_max_height = 14.0f;
 
 inline float const button_label_font_min_height = 11.0f;
-inline float const button_label_font_max_height = 17.0f;
+inline float const button_label_font_max_height = 15.0f;
 
 inline float const group_label_vpad = 7.0f;
 inline float const group_label_font_min_height = 11.0f;
-inline float const group_label_font_max_height = 17.0f;
+inline float const group_label_font_max_height = 15.0f;
 
 } // namespace inf::base::ui
 #endif // INF_BASE_UI_SHARED_CONFIG_HPP
