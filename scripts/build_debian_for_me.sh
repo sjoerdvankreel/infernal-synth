2c5bcfd658bbc4d6ccd9fd55095eeae160c60ece
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-06-14 00:13:42 +0200

    switch juce to submodule

--- scripts/build_debian_for_me.sh
+++ scripts/build_debian_for_me.sh
@@ -1,1 +1,1 @@
-sh ./build_linux.sh /media/sf_hiir_1.40 /media/sf_juce_7.0.5/JUCE /media/sf_vst3sdk_3.7.8/vst3sdk /media/sf_vst3sdk_3.7.8/debian/debug/lib/Debug /media/sf_vst3sdk_3.7.8/debian/release/lib/Release debian
+sh ./build_linux.sh /media/sf_hiir_1.40 /media/sf_vst3sdk_3.7.8/vst3sdk /media/sf_vst3sdk_3.7.8/debian/debug/lib/Debug /media/sf_vst3sdk_3.7.8/debian/release/lib/Release debian
