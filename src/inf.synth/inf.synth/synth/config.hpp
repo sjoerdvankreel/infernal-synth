b91f6ff5340364ccacdc2cd5112e40654896d410
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-06-14 17:33:39 +0200

    restore 10 fx

--- src/inf.synth/inf.synth/synth/config.hpp
+++ src/inf.synth/inf.synth/synth/config.hpp
@@ -9,8 +9,8 @@ inline std::int32_t constexpr vosc_count = 4;
 inline std::int32_t constexpr venv_count = 6;
 inline std::int32_t constexpr vlfo_count = 6;
 inline std::int32_t constexpr glfo_count = 6;
-inline std::int32_t constexpr veffect_count = 8;
-inline std::int32_t constexpr geffect_count = 8;
+inline std::int32_t constexpr veffect_count = 10;
+inline std::int32_t constexpr geffect_count = 10;
 inline std::int32_t constexpr synth_polyphony = 32;
 
 } // namespace inf::synth
