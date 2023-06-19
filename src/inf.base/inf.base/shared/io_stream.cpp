d12c6983193a2c686c4b2e366c21a00a90cfc7f8
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-06-19 21:21:29 +0200

    stuff

--- src/inf.base/inf.base/shared/io_stream.cpp
+++ src/inf.base/inf.base/shared/io_stream.cpp
@@ -1,6 +1,7 @@
 #include <inf.base/shared/io_stream.hpp>
 
 #include <set>
+#include <map>
 #include <vector>
 #include <cassert>
 #include <iostream>
@@ -8,15 +9,26 @@
 
 namespace inf::base {
 
-struct old_param_id
+struct stored_param_id
 {
+  std::int32_t io_type;
   std::string part_guid;
   std::string param_guid;
   std::int32_t part_index;
 };
 
-bool operator<(old_param_id const& l, old_param_id const& r)
-{ return l.part_guid < r.part_guid && l.part_index < r.part_index && l.param_guid < r.param_guid; }
+bool operator<(stored_param_id const& l, stored_param_id const& r)
+{ 
+  if(l.io_type < r.io_type) return true;
+  if(l.io_type > r.io_type) return false;
+  if(l.part_index < r.part_index) return true;
+  if(l.part_index > r.part_index) return false;
+  if(l.part_guid < r.part_guid) return true;
+  if(l.part_guid > r.part_guid) return false;
+  if(l.param_guid < r.param_guid) return true;
+  if(l.param_guid > r.param_guid) return false;
+  return false;
+}
 
 static std::string const magic = "{17026466-059D-4C8C-A13E-510250D72F46}";
 
@@ -98,7 +110,22 @@ io_stream::load(topology_info const& topology, param_value* state)
   topology.init_all_param_defaults(state);
   from_file_version(raw_file_version, old_major, old_minor);
 
-  std::set<old_param_id> unmatched_parameters;
+  // Map stored id to index for new params.
+  std::map<stored_param_id, std::int32_t> new_params;
+  for (std::int32_t rp = 0; rp < topology.input_param_count; rp++)
+  {
+    stored_param_id new_id;
+    std::int32_t part_index = topology.params[rp].part_index;
+    auto const& param = topology.params[rp].descriptor;
+    auto const& part = topology.parts[part_index];
+    new_id.param_guid = param->guid;
+    new_id.part_index = part.type_index;
+    new_id.io_type = param->data.io_type();
+    new_id.part_guid = part.descriptor->guid;
+    new_params[new_id] = rp;
+  }
+
+  std::set<stored_param_id> old_parameters;
   for (std::int32_t sp = 0; sp < param_count; sp++)
   {
     if(!read_string(part_guid)) return false;
@@ -108,11 +135,12 @@ io_stream::load(topology_info const& topology, param_value* state)
     if(io_type < 0 || io_type >= param_io::count) return false;
 
     // Store old identifier. We'll remove it once matched.
-    old_param_id old_id;
+    stored_param_id old_id;
+    old_id.io_type = io_type;
     old_id.part_guid = part_guid;
     old_id.param_guid = param_guid;
     old_id.part_index = type_index;
-    unmatched_parameters.insert(old_id);
+    old_parameters.insert(old_id);
 
     str_value.clear();
     value = param_value();
@@ -131,50 +159,40 @@ io_stream::load(topology_info const& topology, param_value* state)
     default: assert(false); break;
     }
 
-    for (std::int32_t rp = 0; rp < topology.input_param_count; rp++)
-    {
-      std::int32_t part_index = topology.params[rp].part_index;
-      auto const& param = topology.params[rp].descriptor;
-      auto const& part = topology.parts[part_index];
-      if(part_guid != part.descriptor->guid) continue;
-      if(type_index != part.type_index) continue;
-      if(param_guid != param->guid) continue;
-      if(io_type != param->data.io_type()) continue;
+    auto new_id_iter = new_params.find(old_id);
+    if(new_id_iter == new_params.end()) 
+      continue;
+    old_parameters.erase(new_id_iter->first);
+    std::int32_t rp = new_id_iter->second;
+    std::int32_t part_index = topology.params[rp].part_index;
+    auto const& param = topology.params[rp].descriptor;
 
-      // Matched this one.
-      old_param_id matched_old_id;
-      matched_old_id.part_guid = part_guid;
-      matched_old_id.part_index = type_index;
-      matched_old_id.param_guid = param_guid;
-      unmatched_parameters.erase(matched_old_id);
-
-      // Read old stuff.
-      switch (io_type)
-      { 
-      case param_io::real: state[rp].real = value.real; break;
-      case param_io::discrete: state[rp].discrete = value.discrete; break;
-      case param_io::text: if (param->data.parse(true, part_index, str_value.data(), value)) state[rp] = value; break;
-      default: assert(false); break;
-      }
-
-      // Convert to new stuff.
-      state[rp] = topology.convert_param(rp, state[rp], str_value, old_major, old_minor);
-
-      // Make sure we're in range.
-      switch (param->data.type)
-      { 
-      case param_type::real: state[rp].real = std::clamp(state[rp].real, 0.0f, 1.0f); break;
-      default: state[rp].discrete = std::clamp(
-        state[rp].discrete, param->data.discrete.min, 
-        param->data.discrete.effective_max(part_index)); break;
-      }
-
-      break;
+    // Read old stuff.
+    switch (io_type)
+    { 
+    case param_io::real: state[rp].real = value.real; break;
+    case param_io::discrete: state[rp].discrete = value.discrete; break;
+    case param_io::text: if (param->data.parse(true, part_index, str_value.data(), value)) state[rp] = value; break;
+    default: assert(false); break;
     }
+
+    // Convert to new stuff.
+    state[rp] = topology.convert_param(rp, state[rp], str_value, old_major, old_minor);
+
+    // Make sure we're in range.
+    switch (param->data.type)
+    { 
+    case param_type::real: state[rp].real = std::clamp(state[rp].real, 0.0f, 1.0f); break;
+    default: state[rp].discrete = std::clamp(
+      state[rp].discrete, param->data.discrete.min, 
+      param->data.discrete.effective_max(part_index)); break;
+    }
+
+    break;
   }
 
   // Report what's unused by new.
-  for (auto it = unmatched_parameters.begin(); it != unmatched_parameters.end(); ++it)
+  for (auto it = old_parameters.begin(); it != old_parameters.end(); ++it)
     std::cout << "Unused value for " << it->part_guid << " " << it->part_index << ": " << it->param_guid << std::endl;
   topology.state_check(state);
   return true;
