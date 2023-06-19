0162f677e5422b918d27545be89e10e4e411b81c
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-06-19 20:10:50 +0200

    write missing stuff to console

--- src/inf.base/inf.base/shared/io_stream.cpp
+++ src/inf.base/inf.base/shared/io_stream.cpp
@@ -8,6 +8,16 @@
 
 namespace inf::base {
 
+struct old_param_id
+{
+  std::string part_guid;
+  std::string param_guid;
+  std::int32_t part_index;
+};
+
+bool operator<(old_param_id const& l, old_param_id const& r)
+{ return l.part_guid < r.part_guid && l.part_index < r.part_index && l.param_guid < r.param_guid; }
+
 static std::string const magic = "{17026466-059D-4C8C-A13E-510250D72F46}";
 
 static std::uint32_t
@@ -80,54 +90,29 @@ io_stream::load(topology_info const& topology, param_value* state)
   assert(state != nullptr);
   std::uint32_t raw_version = to_file_version(topology.version_major(), topology.version_minor());
 
-  if(!read_string(file_magic) || file_magic != magic)
-  {
-    std::cout << "InfernalSynth: not a patch file." << std::endl;
-    return false;
-  }
-  if(!read_uint32(raw_file_version) || raw_file_version > raw_version)
-  {
-    std::cout << "InfernalSynth: incompatible file format." << std::endl;
-    return false;
-  }
-  if(!read_int32(param_count) || param_count <= 0) 
-  {
-    std::cout << "InfernalSynth: wrong parameter count." << std::endl;
-    return false;
-  }
+  if(!read_string(file_magic) || file_magic != magic) return false;
+  if(!read_uint32(raw_file_version) || raw_file_version > raw_version) return false;
+  if(!read_int32(param_count) || param_count <= 0) return false;
 
   // Set defaults in case some params are missing or invalid.
   topology.init_all_param_defaults(state);
   from_file_version(raw_file_version, old_major, old_minor);
 
-  std::set<std::int32_t> matched_parameter_indices;
+  std::set<old_param_id> unmatched_parameters;
   for (std::int32_t sp = 0; sp < param_count; sp++)
   {
-    if(!read_string(part_guid))
-    {
-      std::cout << "InfernalSynth: failed to read part guid." << std::endl;
-      return false;
-    }
-    if(!read_int32(type_index))
-    {
-      std::cout << "InfernalSynth: failed to read part index." << std::endl;
-      return false;
-    }
-    if(!read_string(param_guid))
-    {
-      std::cout << "InfernalSynth: failed to read parameter guid." << std::endl;
-      return false;
-    }
-    if(!read_int32(io_type))
-    {
-      std::cout << "InfernalSynth: failed to read parameter io type." << std::endl;
-      return false;
-    }
-    if(io_type < 0 || io_type >= param_io::count)
-    {
-      std::cout << "InfernalSynth: invalid parameter io type." << std::endl;
-      return false;
-    }
+    if(!read_string(part_guid)) return false;
+    if(!read_int32(type_index)) return false;
+    if(!read_string(param_guid)) return false;
+    if(!read_int32(io_type)) return false;
+    if(io_type < 0 || io_type >= param_io::count) return false;
+
+    // Store old identifier. We'll remove it once matched.
+    old_param_id old_id;
+    old_id.part_guid = part_guid;
+    old_id.param_guid = param_guid;
+    old_id.part_index = type_index;
+    unmatched_parameters.insert(old_id);
 
     str_value.clear();
     value = param_value();
@@ -135,25 +120,13 @@ io_stream::load(topology_info const& topology, param_value* state)
     switch (io_type)
     {
     case param_io::real: 
-      if (!read_float(value.real))
-      {
-        std::cout << "InfernalSynth: failed to read real parameter value." << std::endl;
-        return false;
-      }
+      if (!read_float(value.real)) return false;
       break;
     case param_io::text:
-      if(!read_string(str_value)) 
-      {
-        std::cout << "InfernalSynth: failed to read text parameter value." << std::endl;
-        return false;
-      }
+      if(!read_string(str_value)) return false;
       break;
     case param_io::discrete: 
-      if(!read_int32(value.discrete)) 
-      {
-        std::cout << "InfernalSynth: failed to read int parameter value." << std::endl;
-        return false;
-      }
+      if(!read_int32(value.discrete)) return false;
       break;
     default: assert(false); break;
     }
@@ -169,7 +142,11 @@ io_stream::load(topology_info const& topology, param_value* state)
       if(io_type != param->data.io_type()) continue;
 
       // Matched this one.
-      matched_parameter_indices.insert(rp);
+      old_param_id matched_old_id;
+      matched_old_id.part_guid = part_guid;
+      matched_old_id.part_index = type_index;
+      matched_old_id.param_guid = param_guid;
+      unmatched_parameters.erase(matched_old_id);
 
       // Read old stuff.
       switch (io_type)
@@ -196,14 +173,9 @@ io_stream::load(topology_info const& topology, param_value* state)
     }
   }
 
-  // Report not-found parameters.
-  for (std::int32_t rp = 0; rp < topology.input_param_count; rp++)
-    if (matched_parameter_indices.find(rp) == matched_parameter_indices.end())
-    {
-      auto const& desc = topology.params[rp].descriptor;
-      std::cout << "Missing value for " << desc->guid << " " << "(" << desc->data.static_name.short_ << ")" << std::endl;
-    }
-
+  // Report what's unused by new.
+  for (auto it = unmatched_parameters.begin(); it != unmatched_parameters.end(); ++it)
+    std::cout << "Unused value for " << it->part_guid << " " << it->part_index << ": " << it->param_guid << std::endl;
   topology.state_check(state);
   return true;
 }
