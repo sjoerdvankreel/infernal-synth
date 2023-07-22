2fa021e60490e2dbdccf4f6305dd2d5a6767da2f
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-07-22 22:40:33 +0200

    faster spectrum take 2

--- src/inf.base/inf.base/shared/spectrum_analyzer.cpp
+++ src/inf.base/inf.base/shared/spectrum_analyzer.cpp
@@ -11,12 +11,21 @@ spectrum_analyzer::analyze(float const* audio, std::size_t count)
 {
   _output.clear();
   float max = 0.0f;
+  std::size_t start_bin = 0;
+  std::size_t bin_count = 1;
   std::vector<std::complex<float>> const& fft = _fft.transform(audio, count);
-  for (std::size_t i = 0; i < fft.size(); i++)
+  while (start_bin < fft.size())
   {
-    float real2 = fft[i].real() * fft[i].real();
-    float imag2 = fft[i].imag() * fft[i].imag();
-    _output.push_back(real2 + imag2);
+    float power = 0.0f;
+    for (std::size_t i = start_bin; i < start_bin + bin_count && i < fft.size(); i++)
+    {
+      float real2 = fft[i].real() * fft[i].real();
+      float imag2 = fft[i].imag() * fft[i].imag();
+      power += real2 + imag2;
+    }
+    _output.push_back(power);
+    start_bin += bin_count;
+    bin_count *= 2;
   }
   for (std::size_t i = 0; i < _output.size(); i++) max = std::max(_output[i], max);
   for (std::size_t i = 0; i < _output.size(); i++) _output[i] = max == 0.0f ? 0.0f : sanity_unipolar(_output[i] / max);
