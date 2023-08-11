d1782f4d0b45f112a651784f9b01c7eb24b691a0
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-11 13:52:06 +0200

    pitch wheel take 14

--- src/inf.base.vst/inf.base.vst/vst_controller.hpp
+++ src/inf.base.vst/inf.base.vst/vst_controller.hpp
@@ -16,9 +16,9 @@ class vst_editor;
 
 // Vst edit controller.
 class vst_controller: 
+public inf::base::plugin_controller,
 public Steinberg::Vst::EditController,
-public Steinberg::Vst::IMidiMapping,
-public inf::base::plugin_controller
+public Steinberg::Vst::IMidiMapping
 {
 protected:
   using FUID = Steinberg::FUID;
@@ -45,6 +45,26 @@ protected:
   vst_controller(std::unique_ptr<inf::base::topology_info>&& topology, FUID const& processor_id);
 
 public:
+  void* current_editor_window() const override;
+  void reload_editor(std::int32_t width) override;
+  void editor_param_changed(std::int32_t index, param_value ui_value) override;
+  std::int32_t editor_current_width() const override { return _editor_width; }
+  void editor_current_width(std::int32_t editor_width) { _editor_width = editor_width; }
+
+  void restart() override;
+  void save_preset(std::string const& path) override;
+  bool load_preset(std::string const& path, bool factory) override;
+  void load_component_state(inf::base::param_value* state) override;
+  void swap_param(std::int32_t source_tag, std::int32_t target_tag) override;
+  void copy_param(std::int32_t source_tag, std::int32_t target_tag) override 
+  { do_edit(target_tag, getParamNormalized(source_tag)); }
+
+  std::string preset_file_extension() override { return "vstpreset"; }
+  std::string default_theme_path(std::string const& plugin_file) const override;
+  std::vector<inf::base::external_resource> themes(std::string const& plugin_file) const override;
+  std::unique_ptr<host_context_menu> host_menu_for_param_index(std::int32_t param_index) const override;
+  std::vector<inf::base::external_resource> factory_presets(std::string const& plugin_file) const override;
+
   tresult PLUGIN_API setState(IBStream* state) override;
   tresult PLUGIN_API getState(IBStream* state) override;
   tresult PLUGIN_API initialize(FUnknown* context) override;
@@ -58,27 +78,6 @@ public:
     DEF_INTERFACE(IMidiMapping)
   END_DEFINE_INTERFACES(EditController)
   REFCOUNT_METHODS(EditController)
-
-  void* current_editor_window() const override;
-  void reload_editor(std::int32_t width) override;
-  void editor_param_changed(std::int32_t index, param_value ui_value) override;
-  std::int32_t editor_current_width() const override { return _editor_width; }
-  void editor_current_width(std::int32_t editor_width) { _editor_width = editor_width; }
-
-  void restart() override;
-  void save_preset(std::string const& path) override;
-  bool load_preset(std::string const& path, bool factory) override;
-  void load_component_state(inf::base::param_value* state) override;
-
-  void swap_param(std::int32_t source_tag, std::int32_t target_tag) override;
-  void copy_param(std::int32_t source_tag, std::int32_t target_tag) override 
-  { do_edit(target_tag, getParamNormalized(source_tag)); }
-
-  std::string preset_file_extension() override { return "vstpreset"; }
-  std::string default_theme_path(std::string const& plugin_file) const override;
-  std::vector<inf::base::external_resource> themes(std::string const& plugin_file) const override;
-  std::unique_ptr<host_context_menu> host_menu_for_param_index(std::int32_t param_index) const override;
-  std::vector<inf::base::external_resource> factory_presets(std::string const& plugin_file) const override;
 };
 
 } // namespace inf::base::vst
