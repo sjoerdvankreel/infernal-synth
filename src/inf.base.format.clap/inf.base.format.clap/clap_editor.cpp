846a31382f268687646247395d9e478b722d288f
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-21 21:09:29 +0200

    clap 39

---
+++ src/inf.base.format.clap/inf.base.format.clap/clap_editor.cpp
@@ -0,0 +1,5 @@
+#include <inf.base.format.clap/clap_editor.hpp>
+
+namespace inf::base::format::clap
+{
+} // inf::base::format::clap
---
+++ src/inf.base.format.clap/inf.base.format.clap/clap_editor.hpp
@@ -0,0 +1,10 @@
+#ifndef INF_BASE_FORMAT_CLAP_CLAP_EDITOR_HPP
+#define INF_BASE_FORMAT_CLAP_CLAP_EDITOR_HPP
+
+#include <inf.base/plugin/plugin_controller.hpp>
+#include <clap/clap.h>
+
+namespace inf::base::format::clap 
+{
+} // inf::base::format::clap
+#endif // INF_BASE_FORMAT_CLAP_CLAP_EDITOR_HPP
