5c4f83db0c0d09265c777aa0d5285a65285f6b22
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-21 20:35:29 +0200

    clap 38

--- src/inf.base.format.clap/inf.base.format.clap/clap_controller.hpp
+++ src/inf.base.format.clap/inf.base.format.clap/clap_controller.hpp
@@ -10,6 +10,23 @@ namespace inf::base::format::clap
 class clap_controller:
 public inf::base::plugin_controller
 {
+public:
+  void* current_editor_window() const override;
+  void reload_editor(std::int32_t width) override;
+  void editor_param_changed(std::int32_t index, param_value ui_value) override;
+
+  void restart() override;
+  void save_preset(std::string const& path) override;
+  bool load_preset(std::string const& path) override;
+  void load_component_state(inf::base::param_value* state) override;
+  void swap_param(std::int32_t source_tag, std::int32_t target_tag) override;
+  void copy_param(std::int32_t source_tag, std::int32_t target_tag) override;
+
+  std::string preset_file_extension();
+  std::string default_theme_path(std::string const& plugin_file) const override;
+  std::vector<inf::base::external_resource> themes(std::string const& plugin_file) const override;
+  std::unique_ptr<host_context_menu> host_menu_for_param_index(std::int32_t param_index) const override;
+  std::vector<inf::base::external_resource> factory_presets(std::string const& plugin_file) const override;
 };
 
 } // inf::base::format::clap
