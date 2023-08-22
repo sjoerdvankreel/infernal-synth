f21aa53ea92731a46dfa169b3c76e10eabb29bb4
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-22 18:30:48 +0200

    clap 60

--- src/inf.plugin.infernal_synth/inf.plugin.infernal_synth/plugin.hpp
+++ src/inf.plugin.infernal_synth/inf.plugin.infernal_synth/plugin.hpp
@@ -2,16 +2,16 @@
 #define IPIS_PLUGIN_HPP
 // macro names cannot be too long for resource compiler
 
-#define IPIS_VERSION "1.3"
+#define IPIS_VERSION "1.4"
 #define IPIS_VERSION_MAJOR 1
-#define IPIS_VERSION_MINOR 3
+#define IPIS_VERSION_MINOR 4
 #define IPIS_VERSION_REVISION 0
 #define IPIS_VENDOR_NAME "Sjoerd van Kreel"
 #define IPIS_VENDOR_MAIL "sjoerdvankreel@gmail.com"
 #define IPIS_VENDOR_URL "https://sjoerdvankreel.github.io/infernal-synth"
 
-#define IPIS_FX_NAME "InfernalSynthFX 1.3"
-#define IPIS_INST_NAME "InfernalSynth 1.3"
+#define IPIS_FX_NAME "InfernalSynthFX 1.4"
+#define IPIS_INST_NAME "InfernalSynth 1.4"
 #define IPIS_FX_UNIQUE_ID "D1D3802692374AB7B6FC3A55A9AE3BCC"
 #define IPIS_INST_UNIQUE_ID "5626A8A247C740E3895EF722E6C1D9C4"
 
