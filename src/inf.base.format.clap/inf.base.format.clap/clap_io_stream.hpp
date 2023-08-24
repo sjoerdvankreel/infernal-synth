87438e5026d6c8b6c33af8352c2683752b149b2f
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-24 15:26:28 +0200

    clap 101

---
+++ src/inf.base.format.clap/inf.base.format.clap/clap_io_stream.hpp
@@ -0,0 +1,59 @@
+#ifndef INF_BASE_FORMAT_CLAP_CLAP_IO_STREAM_HPP
+#define INF_BASE_FORMAT_CLAP_CLAP_IO_STREAM_HPP
+
+#include <inf.base/shared/io_stream.hpp>
+
+#include <vector>
+#include <string>
+#include <cstdint>
+
+namespace inf::base::format::clap {
+
+// CLAP doesnt have a common/wrapper format like
+// vst3. So we basically dump the raw processor/controller
+// values and tuck them together in controller::load/save_preset.
+class clap_io_stream:
+public inf::base::io_stream
+{
+private:
+  std::size_t _pos = 0;
+  std::vector<std::uint8_t> _data = {};
+  
+  template <class T> bool read(T& val)
+  {
+    if(_pos + sizeof(T) >= _data.size()) return false;
+    val = *reinterpret_cast<T*>(_data.data() + _pos);
+    _pos += sizeof(T);
+    return true;
+  }
+
+  template <class T> bool write(T val)
+  {
+    for (std::size_t i = 0; i < sizeof(T); i++)
+      _data.push_back(0);
+    *reinterpret_cast<T*>(_data.data() + _pos) = val;
+    _pos += sizeof(T);
+    return true;
+  }
+
+public:
+  clap_io_stream() {}
+  clap_io_stream(std::uint8_t const* data, std::size_t size)
+  { _data.insert(_data.begin(), data, data + size); }
+
+  std::size_t size() const { return _data.size(); }
+  std::uint8_t const* data() const { return _data.data(); }
+
+  bool read_float(float& val) override { return read(val); }
+  bool read_int32(std::int32_t& val) override { return read(val); }
+  bool read_uint32(std::uint32_t& val) override { return read(val); }
+  bool read_string(std::string& val) override;
+
+  bool write_string(std::string const& val) override;
+  bool write_float(float val) override { return write(val); }
+  bool write_int32(std::int32_t val) override { return write(val); }
+  bool write_uint32(std::uint32_t val) override { return write(val); }
+};
+
+} // namespace inf::base::format::clap
+#endif // INF_BASE_FORMAT_CLAP_CLAP_IO_STREAM_HPP
